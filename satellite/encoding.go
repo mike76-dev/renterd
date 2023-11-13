a4c959ff7f47ee175d6d948541057a038dc7d779
mike76-dev <michael.bulanov@yahoo.com>
2023-11-13 12:03:00 +0100

    Fix update renter settings

--- satellite/encoding.go
+++ satellite/encoding.go
@@ -415,10 +415,10 @@ func (usr *updateSettingsRequest) EncodeToWithoutSignature(e *types.Encoder) {
 	e.WriteBool(usr.BackupFileMetadata)
 	e.WriteBool(usr.AutoRepairFiles)
 	e.WriteBool(usr.ProxyUploads)
-	if usr.AutoRenewContracts || usr.AutoRepairFiles || usr.ProxyUploads {
+	if usr.AutoRenewContracts || usr.BackupFileMetadata || usr.AutoRepairFiles || usr.ProxyUploads {
 		e.WriteBytes(usr.SecretKey)
 	}
-	if usr.AutoRepairFiles || usr.ProxyUploads {
+	if usr.BackupFileMetadata || usr.AutoRepairFiles || usr.ProxyUploads {
 		e.WriteBytes(usr.AccountKey)
 	}
 	if usr.AutoRenewContracts {
