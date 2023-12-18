4186c19b611615ef0769596080c2cff034eb517c
mike76-dev <michael.bulanov@yahoo.com>
2023-12-18 10:40:16 +0100

    Comment test code out

--- cmd/renterd/web.go
+++ cmd/renterd/web.go
@@ -13,11 +13,11 @@ type treeMux struct {
 
 func (t treeMux) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	// For testing only.
-	if origin := req.Header.Get("Origin"); origin != "" {
+	/*if origin := req.Header.Get("Origin"); origin != "" {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	}
+	}*/
 	if req.Method == "OPTIONS" {
 		return
 	}
